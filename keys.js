b2ee97fd449dd6d078d6f6d60172e9a2a3329d93
Brian Bugle <bbugle@sphereoi.com>
2017-07-22 18:30:33 -0400

    Display last 20 tweets

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
 exports.twitterKeys = {
-	consumerKey: '9KphjPVvgY8bmitYznXAs6yts',
-	consumerSecret: 'ikju3YqZmdO27V1SOlz0lMO9KAekzOHja5NUxX4lKhWmHOoW8H',
-	accessTokenKey: '217471931-msaoVBe0dDdaaDLvfu5dMGhwNcaWjyHE4vCpPyOA',
-	accessTokenSecret: 'zizih00psnAYFOJAy2PNpfPewiiUFLaXyYTCKfhpTsn7D',
+	consumer_key: '9KphjPVvgY8bmitYznXAs6yts',
+	consumer_secret: 'ikju3YqZmdO27V1SOlz0lMO9KAekzOHja5NUxX4lKhWmHOoW8H',
+	access_token_key: '217471931-msaoVBe0dDdaaDLvfu5dMGhwNcaWjyHE4vCpPyOA',
+	access_token_secret: 'zizih00psnAYFOJAy2PNpfPewiiUFLaXyYTCKfhpTsn7D',
 }
--- liri.js
+++ liri.js
@@ -1,10 +1,19 @@
-const twitter = require('./keys.js');
+const keys = require('./keys.js');
+const Twitter = require('twitter');
 
 const command = process.argv[2];
 const args = process.argv.splice(3);
 
 if (command === 'my-tweets') {
+	const client = new Twitter(keys.twitterKeys);
+	client.get('statuses/user_timeline', {screen_name: 'brainboogl', count: 20}, function(error, tweets, response) {
 
+		for (let i = 0; i < tweets.length; i++) {
+			console.log('Created: ', tweets[i].created_at);
+			console.log('Tweet: ', tweets[i].text);
+			console.log('');			
+		}
+	});
 } else if (command === 'spotify-this-song') {
 
 } else if (command === 'movie-this') {
