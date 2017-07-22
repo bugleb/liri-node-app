f578cd7cf0ec78f885b29b702c703911d64ee977
Brian Bugle <bbugle@sphereoi.com>
2017-07-22 18:12:55 -0400

    Read in and validate commands

--- liri.js
+++ liri.js
@@ -1,1 +1,16 @@
 const twitter = require('./keys.js');
+
+const command = process.argv[2];
+const args = process.argv.splice(3);
+
+if (command === 'my-tweets') {
+
+} else if (command === 'spotify-this-song') {
+
+} else if (command === 'movie-this') {
+
+} else if (command === 'do-what-it-says') {
+
+} else {
+	console.log(`Invalid command: '${command}'`);
+}
