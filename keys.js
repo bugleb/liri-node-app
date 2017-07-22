5d1fee4b533d2603efb8c159fc60816bd0065e27
Brian Bugle <bbugle@sphereoi.com>
2017-07-22 18:06:12 -0400

    Add twitter keys and access them from liri.js

---
+++ keys.js
@@ -0,0 +1,6 @@
+exports.twitterKeys = {
+	consumerKey: '9KphjPVvgY8bmitYznXAs6yts',
+	consumerSecret: 'ikju3YqZmdO27V1SOlz0lMO9KAekzOHja5NUxX4lKhWmHOoW8H',
+	accessTokenKey: '217471931-msaoVBe0dDdaaDLvfu5dMGhwNcaWjyHE4vCpPyOA',
+	accessTokenSecret: 'zizih00psnAYFOJAy2PNpfPewiiUFLaXyYTCKfhpTsn7D',
+}
