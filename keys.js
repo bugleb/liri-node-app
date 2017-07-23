d60289e72d496ef0b5a995d43337970b506a5f2a
Brian Bugle <bbugle@sphereoi.com>
2017-07-22 20:58:22 -0400

    Add code for spotify

--- keys.js
+++ keys.js
@@ -4,3 +4,8 @@ exports.twitterKeys = {
 	access_token_key: '217471931-msaoVBe0dDdaaDLvfu5dMGhwNcaWjyHE4vCpPyOA',
 	access_token_secret: 'zizih00psnAYFOJAy2PNpfPewiiUFLaXyYTCKfhpTsn7D',
 }
+
+exports.spotifyKeys = {
+	id: '578a9bcc27f74b41b1086cfbbba23ece',
+	secret: '90e94c27ba9042e4895f91c96dc298ff',
+}
--- liri.js
+++ liri.js
@@ -1,5 +1,6 @@
 const keys = require('./keys.js');
 const Twitter = require('twitter');
+const Spotify = require('node-spotify-api');
 
 const command = process.argv[2];
 const args = process.argv.splice(3);
@@ -7,7 +8,6 @@ const args = process.argv.splice(3);
 if (command === 'my-tweets') {
 	const client = new Twitter(keys.twitterKeys);
 	client.get('statuses/user_timeline', {screen_name: 'brainboogl', count: 20}, function(error, tweets, response) {
-
 		for (let i = 0; i < tweets.length; i++) {
 			console.log('Created: ', tweets[i].created_at);
 			console.log('Tweet: ', tweets[i].text);
@@ -15,7 +15,29 @@ if (command === 'my-tweets') {
 		}
 	});
 } else if (command === 'spotify-this-song') {
+	const client = new Spotify(keys.spotifyKeys);
+	const songName = (args.length === 0) ? 'The Sign' : args[0];
 
+	client.search({ type: 'track', query: songName }, function(err, data) {
+		if (err) {
+			return console.log('An error occurred: ', err);
+		}
+
+		for (let i = 0; i < data.tracks.items.length; i++) {
+			const song = data.tracks.items[i];
+
+			const artists = [];
+			for (let j = 0; j < song.artists.length; j++) {
+				artists.push(song.artists[j].name);
+			}
+
+			console.log('Artist(s): ', artists.join(', '));
+			console.log('Song Name: ', song.name);
+			console.log('Preview: ', song.preview_url);
+			console.log('Album: ', song.album.name);
+			console.log('');
+		}
+	});
 } else if (command === 'movie-this') {
 
 } else if (command === 'do-what-it-says') {
